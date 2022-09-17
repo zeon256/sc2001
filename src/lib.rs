b199d2c16f3ddf54269b30918b4187b317b2e30e
Budi Syahiddin <me@inve.rs>
2022-09-17 22:00:06 +0800

    🔒️ Added code to generate csv for bench

--- src/lib.rs
+++ src/lib.rs
@@ -67,31 +67,32 @@ pub mod test {
 
     #[test]
     fn export_all_data_to_csv() {
-        let mut all_estimates = String::from("sz,mean,median\n");
-        for sz in 3..=256 {
-            let mut buf = String::new();
-            let file_name = format!(
-                "target/criterion/insertion_merge_sort(100k_s{})/new/estimates.json",
-                sz
-            );
-            // let file_name = format!("target/criterion/xd/new/estimates.json");
-            println!("{file_name}");
-            let mut f = File::open(file_name).unwrap();
-            let _ = f.read_to_string(&mut buf).unwrap();
-            let estimates = serde_json::from_str::<Estimates>(&buf).unwrap();
-            let (mean, median) = (
-                estimates.mean.point_estimate,
-                estimates.median.point_estimate,
-            );
-            all_estimates.push_str(&format!(
-                "{},{},{}\n",
-                sz,
-                mean / 1000000.0,
-                median / 1000000.0
-            ));
-            // all_estimates.push((sz, estimates.mean.point_estimate, estimates.median.point_estimate));
+        let array_sz = ["1k", "10k", "100k", "500k", "1mill"];
+
+        for name in array_sz {
+            let mut all_estimates = String::from("sz,mean,median\n");
+            for sz in 3..=128 {
+                let mut buf = String::new();
+                let file_name = format!(
+                    "target/criterion/insertion_merge_sort({name}_s{sz})/new/estimates.json"
+                );
+
+                println!("{file_name}");
+
+                let mut f = File::open(file_name).unwrap();
+                let _ = f.read_to_string(&mut buf).unwrap();
+
+                let estimates = serde_json::from_str::<Estimates>(&buf).unwrap();
+                let (mean, median) = (
+                    estimates.mean.point_estimate / 1000000.0,
+                    estimates.median.point_estimate / 1000000.0,
+                );
+
+                all_estimates.push_str(&format!("{sz},{mean},{median}\n",));
+            }
+
+            let mut f = File::create(format!("bench_{name}.csv")).unwrap();
+            f.write_all(all_estimates.as_bytes()).unwrap();
         }
-        let mut f = File::create("bench_100k.csv").unwrap();
-        f.write_all(all_estimates.as_bytes()).unwrap();
     }
 }
